ce73927a1dfb525b98e82c5c3405ad271fc6da04
Georgi 7DIGIT <georgi@7digit.io>
2023-02-08 01:25:37 +0200

    Create: getMultipleProvidersDataByIDs query

--- service/queries/providers.js
+++ service/queries/providers.js
@@ -9,3 +9,16 @@ export const getProviderDataById = async ({ providerId, poolCountry }) =>
     `,
     [providerId]
   );
+
+export const getMultipleProvidersDataByIDs = async ({
+  poolCountry,
+  providerDetailIds,
+}) =>
+  await getDBPool("piiDb", poolCountry).query(
+    `
+      SELECT name, surname, patronym, email, provider_detail_id
+      FROM provider_detail
+      WHERE provider_detail_id = ANY($1);
+    `,
+    [providerDetailIds]
+  );
