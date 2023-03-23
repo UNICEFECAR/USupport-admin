b7319eadd96967e3b50857f0b1783e503d259c5f
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2023-03-24 00:50:07 +0200

    Add: russian and kazakh translations

---
+++ service/translations/kk.js
@@ -0,0 +1,22 @@
+export default {
+  platform_not_error: "Платформа табылмады",
+  password_validation_error:
+    "Кілтсөз кемінде 8 таңбадан, 1 бас әріптен, 1 кіші әріптен және 1 саннан тұруы керек.",
+  admin_not_found_error: "Әкімші табылмады",
+  email_already_used_error: "Бұл электронды пошта әлдеқашан қолданылып жатыр",
+  incorrect_email_error: "Осындай электронды поштасы бар әкімші жоқ",
+  incorrect_password_error: "Берілген кілтсөз дұрыс емес",
+  not_authenticated_error:
+    "Бұл ресурсқа қол жеткізу үшін әкімші аутентификациядан өтуі керек",
+  invalid_refresh_token_error: "Жаңарту токені жарамсыз немесе әлдеқашан қолданылып жатыр",
+  invalid_reset_password_token_error: "Кілтсөзді өзгерту токені жарамсыз немесе мерзімі өтіп кеткен",
+  account_deactivated_error: "Сіздің аккаунтыңыз істен шығарылды",
+  invalid_admin_otp_error: "Invalid OTP",
+  too_many_otp_requests_error:
+    "Too many OTP requests made, please try again soon.",
+  sponsor_email_already_exists_error:
+    "A sponsor with this email already exists.",
+  campaign_code_already_exists_error:
+    "A campaign with this coupon code already exists",
+  campaign_not_found_error: "Campaign not found",
+};
