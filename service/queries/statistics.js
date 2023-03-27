4c77a1644689e7ce0abbd961f16bfbeaaa8e3d35
Georgi 7DIGIT <georgi@7digit.io>
2023-03-27 19:24:07 +0300

    Add: the consultation's created_at field to the select clause of the getProviderStatisticsQuery

--- service/queries/statistics.js
+++ service/queries/statistics.js
@@ -79,7 +79,7 @@ export const getContactFormsQuery = async ({ poolCountry }) =>
 export const getProviderStatisticsQuery = async ({ poolCountry, providerId }) =>
   await getDBPool("clinicalDb", poolCountry).query(
     `  
-      SELECT client_detail_id, provider_detail_id, time, status, price, type, consultation.campaign_id
+      SELECT client_detail_id, provider_detail_id, time, status, price, type, consultation.campaign_id, consultation.created_at
       FROM consultation
         INNER JOIN transaction_log ON consultation.consultation_id = transaction_log.consultation_id
       WHERE provider_detail_id = $1 AND (status = 'finished' OR (status = 'scheduled' AND now() > time + interval '1 hour'))
