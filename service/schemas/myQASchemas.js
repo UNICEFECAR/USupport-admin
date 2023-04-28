8813c7de25e0812cd97d6723d46b057333b697aa
Georgi 7DIGIT <georgi@7digit.io>
2023-04-28 17:50:14 +0300

    Create: request to get answered/unanswered questions in th MyQA

--- service/schemas/myQASchemas.js
+++ service/schemas/myQASchemas.js
@@ -9,3 +9,8 @@ export const deleteQuestionSchema = getQuestionReportsSchema.shape({
   admin_id: yup.string().uuid().required(),
   questionId: yup.string().uuid().required(),
 });
+
+export const getAllQuestionsSchema = yup.object().shape({
+  country: yup.string().required(),
+  type: yup.string().oneOf(["answered", "unanswered"]).required(),
+});
