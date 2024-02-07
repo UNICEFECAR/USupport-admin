0e021f5f42d22300729c2c089961b3e078a79847
Georgi 7DIGIT <georgi@7digit.io>
2024-02-07 15:19:22 +0200

    Refactor: the generate forgot password link to be a post request

--- service/routes/v1/rescueRouter.js
+++ service/routes/v1/rescueRouter.js
@@ -12,39 +12,38 @@ import {
 
 const router = express.Router();
 
-router
-  .route("/forgot-password")
-  .get(async (req, res, next) => {
-    /**
-     * #route   GET /admin/v1/rescue/forgot-password
-     * #desc    Send forgot password email
-     */
-    const language = req.header("x-language-alpha-2");
-    const { email, role } = req.query;
+router.route("/forgot-password-link").post(async (req, res, next) => {
+  /**
+   * #route   POST /admin/v1/rescue/forgot-password-link
+   * #desc    Send forgot password email
+   */
+  const language = req.header("x-language-alpha-2");
+  const { email, role } = req.body;
 
-    return await initForgotPasswordSchema
-      .noUnknown(true)
-      .strict(true)
-      .validate({ language, email, role })
-      .then(sendForgotPasswordEmail)
-      .then((result) => res.status(200).send(result))
-      .catch(next);
-  })
-  .post(async (req, res, next) => {
-    /**
-     * #route   POST /admin/v1/rescue/forgot-password
-     * #desc    Reset forgot password with token
-     */
-    const language = req.header("x-language-alpha-2");
-    const payload = req.body;
+  return await initForgotPasswordSchema
+    .noUnknown(true)
+    .strict(true)
+    .validate({ language, email, role })
+    .then(sendForgotPasswordEmail)
+    .then((result) => res.status(200).send(result))
+    .catch(next);
+});
 
-    return await resetForgotPasswordSchema
-      .noUnknown(true)
-      .strict(true)
-      .validate({ ...payload, language })
-      .then(resetForgotPassword)
-      .then((result) => res.status(200).send(result))
-      .catch(next);
-  });
+router.route("/forgot-password").post(async (req, res, next) => {
+  /**
+   * #route   POST /admin/v1/rescue/forgot-password
+   * #desc    Reset forgot password with token
+   */
+  const language = req.header("x-language-alpha-2");
+  const payload = req.body;
+
+  return await resetForgotPasswordSchema
+    .noUnknown(true)
+    .strict(true)
+    .validate({ ...payload, language })
+    .then(resetForgotPassword)
+    .then((result) => res.status(200).send(result))
+    .catch(next);
+});
 
 export { router };
