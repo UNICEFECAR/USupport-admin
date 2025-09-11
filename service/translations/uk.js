f502155daf1c219aa0478d8bcb7b9c45f802c61d
7Digit-main <it-support@7digit.io>
2025-09-11 15:06:24 +0300

    Refactor: uk transations

--- service/translations/uk.js
+++ service/translations/uk.js
@@ -1,30 +1,31 @@
 export default {
-  platform_not_error: "Платформу не знайдено",
+  platform_not_error: "Платформи не знайдено",
   password_validation_error:
     "Пароль має містити щонайменше 8 символів, 1 велику літеру, 1 малу літеру і 1 цифру",
-  admin_not_found_error: "Адміністратора(ки) не знайдено",
+  admin_not_found_error: "Адміністратора/ки не знайдено",
   email_already_used_error: "Ця електронна адреса вже використовується",
-  incorrect_email_error: "За цією адресою немає адміністратора(ки)",
+  incorrect_email_error: "Немає адміністратора/ки з такою електронною поштой",
   incorrect_password_error: "Наданий пароль є невірним",
   incorrect_credentials_error: "Неправильні облікові дані",
   not_authenticated_error:
-    "Щоб отримати доступ до цього ресурсу, адміністратор(ка) має пройти автентифікацію",
-  invalid_refresh_token_error: "Оновлений токен недійсний або вже використаний",
+    "Щоб отримати доступ до цього ресурсу, адміністратор/ка має пройти автентифікацію",
+  invalid_refresh_token_error: "Токен оновлення недійсний або вже використаний",
   invalid_reset_password_token_error:
-    "Недійсний або прострочений токен скидання пароля. Будь ласка, запросіть новий за адресою: ",
-  account_deactivated_error: "Ваш обліковий запис деактивовано",
-  invalid_admin_otp_error: "Недійсний OП",
+    "Недійсний або прострочений токен для скидання пароля. Запитай новий за адресою:",
+  account_deactivated_error: "Твій обліковий запис деактивовано",
+  invalid_admin_otp_error: "Недійсний OTP",
   too_many_otp_requests_error:
-    "Занадто багато OП-запитів, повторіть спробу найближчим часом.",
-  sponsor_email_already_exists_error: "Спонсор(ка) із такою адресою вже існує.",
+    "Занадто багато OTP запитів, повтори спробу найближчим часом.",
+  sponsor_email_already_exists_error:
+    "Спонсор із такою електронною поштою вже існує.",
   campaign_code_already_exists_error: "Кампанія з цим кодом купона вже існує",
-  campaign_not_found_error: "Кампанію не знайдено",
-  provider_not_found_error: "Консультанта/тку не знайдено",
-  question_cant_be_deleted_error: "Запитання не можна видалити",
+  campaign_not_found_error: "Кампанії не знайдено",
+  provider_not_found_error: "Консультанта/ки не знайдено",
+  question_cant_be_deleted_error: "Не можна видалити запитання",
   organization_exists_error: "Організація вже існує",
   provider_already_assigned_to_org:
-    "Консультант/ка вже призначений/на для цієї організації",
-  organization_not_found_error: "Організацію не знайдено",
+    "Консультант/ка вже призначений/а для цієї організації",
+  organization_not_found_error: "Організації не знайдено",
   organization_geolocation_not_found_error:
     "Геолокацію організації не знайдено",
   baseline_assessment_threshold_not_found_error: "Шум зони не знайдено",
